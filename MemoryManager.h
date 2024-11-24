7218b4c8ce9d3e8cabd2a29d4589ac7d6a1034f2
Judy-Chua <135433594+Judy-Chua@users.noreply.github.com>
2024-11-25 00:00:30 +0800

    Update MemoryManager.h (Arwyn and Judy)
    fix header file to make this consistent with cpp file
    
    manually added to main since this seems good based on logic but not yet tested and will not break code. More changes can be seen in mco2-draft. Will continue this tomorrow

--- MemoryManager.h
+++ MemoryManager.h
@@ -7,32 +7,33 @@
 
 struct Proc {
     int pid;               // Process ID
-    int memoryStart;       // Start address in memory
-    int memoryEnd;         // End address in memory
+    int memory;            // Process size
     bool active;           // Indicates if the process is in memory
+    long long time;        // Indicates how long this process has been residing in the memory
 };
 
 class MemoryManager {
 private:
     std::vector<int> memory;            // Memory is represented as an array of frames
     std::vector<Proc> processes;
-    int totalFragmentation = 0;
+    int totalFragmentation = 0; //might remove since there is nothing in specs that asks for this
     int availableMemory = -1; // default value just for initialization
 
     int maxMemory = 16384;  // Total memory available (16KB)
-    int memPerProc = 4096; // Memory required per process (4KB)
     int frameSize = 16;     // Frame size (16 bytes)
-    int minMemPerProc = -1;
-    int maxMemPerProc = -1;
+
+    std::string memType;
+
+    bool flatAllocate(int pid, int processSize);
+    bool pagingAllocate(int pid, int processSize);
+    void deallocateOldest();
 
 public:
-    MemoryManager(int maxMemory, int frameSize, int minMemPerProc, int maxMemPerProc, int availableMemory);
-    bool allocateMemory(int pid);
+    MemoryManager(int maxMemory, int frameSize, int availableMemory);
+    bool allocateMemory(int pid, int processSize);
     bool isAllocated(int pid);
     void deallocateMemory(int pid);
-    void printMemoryLayout(int cycle) const;
     int getActiveProcessesCount() const;
-    std::string getCurrentTime() const;
     int getAvailableMemory() const;
     void setAvailableMemory(int free);
 
