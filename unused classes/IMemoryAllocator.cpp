93394b205353d96e90d93a18439f9ec7f676e580
Judy-Chua <135433594+Judy-Chua@users.noreply.github.com>
2024-11-25 23:22:00 +0800

    Delete unused classes directory

--- unused classes/IMemoryAllocator.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-class IMemoryAllocator {
-public:
-	virtual void* allocate(size_t size) = 0;
-	virtual void deallocate(void* ptr) = 0;
-	virtual std::string visualizeMemory() = 0;
-};
